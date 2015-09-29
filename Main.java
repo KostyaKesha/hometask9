9e9e7a460c054ad3aa1707102e517e596baf8ed7
KostyaKesha <steadyk79@gmail.com>
2015-09-29 19:36:27 +0300

    Rejected commit: Default

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> cats = new ArrayList();
+        List<String> cats = new ArrayList<String>();
         // ∆или три кот€ры
         cats.add("Barsik");
         cats.add("Murzik");
